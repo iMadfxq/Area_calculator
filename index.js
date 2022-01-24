59afb2f090ea8b996c9ab64d92fac3ac84151bea
iMadfxq <imadfxq@gmail.com>
2022-01-23 23:57:30 -0600

    Finished basic logic of the project

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const input = document.querySelector('#radius_input')
-let radius = null
+let input = document.querySelector('#radius_input')
+let radius = 0
 input.addEventListener('change', () => {radius = input.value})
 
-const button = document.querySelector('#button')
-const area = Math.PI * radius**2
+let root = document.querySelector('#root')
+
+let button = document.querySelector('#button')
+
+let area = () => {
+  let a = Math.trunc(Math.PI * radius ** 2)
+  return a
+}
+function alerta() {
+  let p = document.createElement('p')
+  p.textContent = `the area of your circle is ${area()}`
+  if (root.innerHTML.includes('<p>')){
+    let old = root.querySelector('p')
+    old.remove()
+    root.appendChild(p)
+  } else {
+    root.appendChild(p)
+  }
+}
 
 
-button.addEventListener('click', )
+button.addEventListener('click', alerta)
